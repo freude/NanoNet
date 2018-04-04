a30c5ae57302d28863e4da968de13f5117bb60b2
Mykhailo Klymenko <mike.klymenko@rmit.edu.au>
2018-04-04 10:04:29 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='tb', 
+      version='0.1',
+      description='Python framework for the tight-binding computations',
+      author='Mike Klymenko',
+      author_email='mike.klymenko@rmit.edu.au',
+      license='MIT',
+      packages=['tb'],
+      scripts=['tb/tb'],
+      zip_safe=False
+      )
