6058faab1da9c7340f09c8d50757b758bf1c8f0a
sglambert <sam.gervase.lambert@gmail.com>
2018-08-24 10:24:24 +1000

    SiNWtest changes - code in loop

--- SiNWtest.py
+++ SiNWtest.py
@@ -24,42 +24,25 @@ for xyz_file in os.listdir(path):
         plt.savefig('hamitonian.pdf')
 
         a_si = 5.50
-        PRIMITIVE_CELL = [[0, 0, 0.5 * a_si]]
+        PRIMITIVE_CELL = [[0, 0, a_si]]
         hamiltonian.set_periodic_bc(PRIMITIVE_CELL)
-        k = np.linspace(0.0, 100, 100)
-        k = np.vstack((np.zeros(k.shape), np.zeros(k.shape), k)).T
 
-        vals = np.zeros((k.shape[0], hamiltonian.h_matrix.shape[0]), dtype=np.complex)
+        num_points = 200
+        kk = np.linspace(0, 3.14 / 2, num_points, endpoint=True)
 
-        for jj, i in enumerate(k):
-            vals[jj, :], _ = hamiltonian.diagonalize_periodic_bc(list(i))
+ # Band structure computation for each K-point
 
-        import matplotlib.pyplot as plt
-        plt.plot(np.sort(np.real(vals)))
+        band_structure = []
+
+        for jj in xrange(num_points):
+            vals, _ = hamiltonian.diagonalize_periodic_bc([0, 0, kk[jj]])
+            band_structure.append(vals)
+
+        band_structure = np.array(band_structure)
+
+        ax = plt.axes()
+        ax.set_title('Band structure of Silicon Nanowire')
+        ax.set_xlabel(r'Wave vector (k)')
+        ax.set_ylabel(r'Energy (eV)')
+        ax.plot(kk, np.sort(np.real(band_structure)))
         plt.show()
-
-# Determining K-points where we want to compute band structure
-
-num_points = 200
-kk = np.linspace(0, 3.14/2, num_points, endpoint=True)
-
-#Band structure computation for each K-point
-
-band_structure = []
-
-for jj in xrange(num_points):
-    vals, _ = hamiltonian.diagonalize_periodic_bc([0, 0, kk[jj]])
-    band_structure.append(vals)
-
-band_structure = np.array(band_structure)
-
-from matplotlib import rc
-
-plt.rc('text', usetex=True)
-plt.rc('font', family='sans-serif', size=12)
-ax = plt.axes()
-ax.set_title('Band structure of Silicon Nanowire')
-ax.set_xlabel(r'Wave vector (k)')
-ax.set_ylabel(r'Energy (eV)')
-ax.plot(kk, np.sort(np.real(band_structure)))
-plt.show()
