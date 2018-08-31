90967f6c52fe5d0ef829d4ac1d55763ef7ab6f9d
sglambert <sam.gervase.lambert@gmail.com>
2018-08-31 11:37:23 +1000

    updated HamiltonianSiNW script

--- hamiltonianSiNW.py
+++ hamiltonianSiNW.py
@@ -1,6 +1,5 @@
 import os
 import tb
-
 """ get_h_matrix computes the hamiltonian matrix for the xyz coordinate file
     in path. """
 
@@ -8,12 +7,12 @@ path = 'c:\users\sammy\desktop\NanoNet\input_samples\SiNW2.xyz'
 
 
 def get_h_matrix(path):
-            hamiltonian = tb.Hamiltonian(xyz=os.path.join(path, xyz_file), nn_distance=2.4)
-            hamiltonian.initialize()
-            a_si = 5.50
-            PRIMITIVE_CELL = [[0, 0, a_si]]
-            hamiltonian.set_periodic_bc(PRIMITIVE_CELL)
-            return hamiltonian
+    hamiltonian = tb.Hamiltonian(xyz=path, nn_distance=2.4)
+    hamiltonian.initialize()
+    a_si = 5.50
+    PRIMITIVE_CELL = [[0, 0, a_si]]
+    hamiltonian.set_periodic_bc(PRIMITIVE_CELL)
+    return hamiltonian
 
 
 SiNW2 = get_h_matrix(path)
