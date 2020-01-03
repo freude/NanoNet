c9c7be80eef5f7a2fd260d5f8d448b6bbac87b11
Mykhailo Klymenko <misha.klymenko@gmail.com>
2020-01-04 00:31:13 +1100

    Update test_block_tridiagonalization.py

--- test/test_block_tridiagonalization.py
+++ test/test_block_tridiagonalization.py
@@ -2,7 +2,7 @@ import unittest
 import doctest
 import tb.block_tridiagonalization as btd
 suite = doctest.DocTestSuite(btd)
-
+unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == "__main__":
     unittest.TextTestRunner(verbosity=2).run(suite)
