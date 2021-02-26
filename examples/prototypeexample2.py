2c8d26d60ddd351171f3a88942758945770fdaae
Jesse <jesse.vaitkus@gmail.com>
2021-02-26 16:07:01 +1100

    Computes singular values of broadening matrix, useful for Model Order Reduction.

--- examples/prototypeexample2.py
+++ examples/prototypeexample2.py
@@ -12,41 +12,64 @@ tb.Orbitals.orbital_sets = {'A': a}
 
 tb.set_tb_params(PARAMS_A_A={'ss_sigma': -0.5})
 
-xyz_file = """1
+damp_value = 0.05j  # Large damp value 0.05 to provide noticeable smearing
+
+xyz_file2 = """3
 A cell
 A1       0.0000000000    0.0000000000    0.0000000000
+A2       1.0000000000    0.0000000000    0.0000000000
+A3       2.0000000000    0.0000000000    0.0000000000
 """
 
-h = tb.Hamiltonian(xyz=xyz_file, nn_distance=1.1)
+h = tb.Hamiltonian(xyz=xyz_file2, nn_distance=1.1)
 h.initialize()
 h.set_periodic_bc([[0, 0, 1.0]])
 h_l, h_0, h_r = h.get_hamiltonians()
 
-energy = np.linspace(-2.5, 0.5, 716)
+energy = np.linspace(-4.5, 2.5, 716)
 
 sgf_l = []
 sgf_r = []
 
-sgf2_l = []
-sgf2_r = []
+gam_l_u = []  #We won't store Gamma, we'll store their decomposition.
+gam_l_s = []
+gam_l_vh = []
+gam_r_u = []
+gam_r_s = []
+gam_r_vh = []
 
 for E in energy:
     # Note that though the surface Green's function technique is very fast, it can
     # have slight errors due to choice of numerical cutoffs, if the solution is
     # accurate the simple iterative will return the same answer immediately
-    sf = surface_greens_function(E, h_l, h_0, h_r, damp=0.001j)
+    sf = surface_greens_function(E, h_l, h_0, h_r, damp=damp_value)
     L = sf[0]
     R = sf[1]
 
-    L = simple_iterative_greens_function(E, h_l, h_0, h_r, damp=0.001j, initialguess=L)
-    R = simple_iterative_greens_function(E, h_r, h_0, h_l, damp=0.001j, initialguess=R)
+    L = simple_iterative_greens_function(E, h_l, h_0, h_r, damp=damp_value, initialguess=L)
+    R = simple_iterative_greens_function(E, h_r, h_0, h_l, damp=damp_value, initialguess=R)
+
+    ul, sl, vhl = np.linalg.svd( 1j*( L - L.conj().T ) )
+    ur, sr, vhr = np.linalg.svd( 1j*( R - R.conj().T ) )
 
     sgf_l.append(L)
     sgf_r.append(R)
+    gam_l_u.append(ul)
+    gam_l_s.append(sl)
+    gam_l_vh.append(vhl)
+    gam_r_u.append(ur)
+    gam_r_s.append(sr)
+    gam_r_vh.append(vhr)
 
 
 sgf_l = np.array(sgf_l)
 sgf_r = np.array(sgf_r)
+gam_l_u = np.array(gam_l_u)
+gam_l_s = np.array(gam_l_s)
+gam_l_vh = np.array(gam_l_vh)
+gam_r_u = np.array(gam_r_u)
+gam_r_s = np.array(gam_r_s)
+gam_r_vh = np.array(gam_r_vh)
 
 
 num_sites = h_0.shape[0]
@@ -72,5 +95,19 @@ axs[1].plot(energy, tr, 'k')
 axs[1].set_xlabel('Energy (eV)')
 axs[1].set_ylabel('Transmission probability')
 plt.show(block=False)
+
+
+fig, axs = plt.subplots(2, figsize=(5, 7))
+fig.suptitle('Green\'s function technique')
+axs[0].plot(energy, gam_l_s, 'k')
+# axs[0].title.set_text('Density of states')
+axs[0].set_xlabel('Energy (eV)')
+axs[0].set_ylabel('Singular Values (left)')
+axs[1].plot(energy, gam_r_s, 'k')
+# axs[1].title.set_text('Transmission function')
+axs[1].set_xlabel('Energy (eV)')
+axs[1].set_ylabel('Singular Values (right)')
+plt.show(block=False)
+
 
 plt.show()
