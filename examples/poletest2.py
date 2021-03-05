374d8d75b53f85d35c7859c783bf2893c9faffb5
Jesse <jesse.vaitkus@gmail.com>
2021-03-05 14:20:40 +1100

    Added in numerical examples, solutions agree up to a factor of two that is missing somewhere.

--- examples/poletest2.py
+++ examples/poletest2.py
@@ -5,6 +5,8 @@ import nanonet.tb as tb
 from nanonet.negf.greens_functions import simple_iterative_greens_function, sancho_rubio_iterative_greens_function, \
     surface_greens_function
 from nanonet.negf import pole_summation_method
+from nanonet.negf.pole_summation_method import fermi_fun
+
 
 a = tb.Orbitals('A')
 a.add_orbital('s', 0)
@@ -37,7 +39,18 @@ h.initialize()
 h.set_periodic_bc([[0, 0, 1.0]])
 h_l, h_0, h_r = h.get_hamiltonians()
 
-energy = np.linspace(-3.975, -3.825, 211)
+muL = -3.92
+muR = -3.90
+muC = 0.5*(muL + muR)
+# muL = -3.915
+# muR = -3.905
+kT = 0.010
+reltol = 10**-6
+p = np.ceil( -np.log(reltol) )
+
+numE = round((muR - muL + 2*p*kT)/(0.05*kT)) + 1
+
+energy = np.linspace(muL - p*kT, muR + p*kT, numE)
 
 sgf_l = []
 sgf_r = []
@@ -46,12 +59,12 @@ for E in energy:
     # Note that though the surface Green's function technique is very fast, it can
     # have slight errors due to choice of numerical cutoffs, if the solution is
     # accurate the simple iterative will return the same answer immediately
-    sf = surface_greens_function(E, h_l, h_0, h_r, damp=0.001j)
+    sf = surface_greens_function(E, h_l, h_0, h_r, damp=0.0001j)
     L = sf[0]
     R = sf[1]
 
-    L = simple_iterative_greens_function(E, h_l, h_0, h_r, damp=0.001j, initialguess=L)
-    R = simple_iterative_greens_function(E, h_r, h_0, h_l, damp=0.001j, initialguess=R)
+    L = simple_iterative_greens_function(E, h_l, h_0, h_r, damp=0.0001j, initialguess=L)
+    R = simple_iterative_greens_function(E, h_r, h_0, h_l, damp=0.0001j, initialguess=R)
 
     sgf_l.append(L)
     sgf_r.append(R)
@@ -63,16 +76,24 @@ sgf_r = np.array(sgf_r)
 
 num_sites = h_0.shape[0]
 gf = np.linalg.pinv(np.multiply.outer(energy, np.identity(num_sites)) - h_0 - sgf_l - sgf_r)
-dos = -np.trace(np.imag(gf), axis1=1, axis2=2) # should be anti-hermitian part, sloppy.
-tr = np.zeros((energy.shape[0]), dtype=complex)
+val01 = np.zeros(h_0.shape[0])
+val02 = np.zeros(h_0.shape[0])
+val03 = np.zeros(h_0.shape[0])
 
 for j, E in enumerate(energy):
     gf0 = gf[j, :, :]
-    gamma_l = 1j * (sgf_l[j, :, :] - sgf_l[j, :, :].conj().T)
-    gamma_r = 1j * (sgf_r[j, :, :] - sgf_r[j, :, :].conj().T)
-    tr[j] = np.real(np.trace(np.linalg.multi_dot([gamma_l, gf0, gamma_r, gf0.conj().T])))
-    dos[j] = np.real(np.trace(1j * (gf0 - gf0.conj().T)))
+    val01 = val01 + np.diag(gf0)*(fermi_fun(E, muR, kT) - fermi_fun(E, muL, kT))
+    val02 = val02 + np.diag(gf0)*(fermi_fun(E, muR, kT) - fermi_fun(E, muC, kT))
+    val03 = val03 + np.diag(gf0)*(fermi_fun(E, muC, kT) - fermi_fun(E, muL, kT))
 
+
+# for j, E in enumerate(energy):
+#     gf0 = gf[j, :, :]
+#     gamma_l = 1j * (sgf_l[j, :, :] - sgf_l[j, :, :].conj().T)
+#     gamma_r = 1j * (sgf_r[j, :, :] - sgf_r[j, :, :].conj().T)
+#     tr[j] = np.real(np.trace(np.linalg.multi_dot([gamma_l, gf0, gamma_r, gf0.conj().T])))
+#     dos[j] = np.real(np.trace(1j * (gf0 - gf0.conj().T)))
+#
 # fig, axs = plt.subplots(2, figsize=(5, 7))
 # fig.suptitle('Green\'s function technique')
 # axs[0].plot(energy, dos, 'k')
@@ -85,12 +106,7 @@ for j, E in enumerate(energy):
 # axs[1].set_ylabel('Transmission probability')
 # plt.show(block=False)
 
-muL = -3.92
-muR = -3.90
-#muL = -3.915
-#muR = -3.905
-kT = 0.010
-reltol = 10**-6
+
 
 poles, residuesL, residuesR = pole_summation_method.pole_finite_difference(muL, muR, kT, reltol)
 
@@ -137,10 +153,22 @@ for j, E in enumerate(poles):
     val3 = val3 + 0.5*(residuesR[j] + residuesL[j])*np.diag(gf00)
     val4 = val4 + 0.5*(residuesR[j] - residuesL[j])*np.diag(gf00)
 
+val01 = -2*np.imag(val01)/(muR-muL)*(energy[1]-energy[0])*.5
+val02 = -2*np.imag(val02)/(muR-muC)*(energy[1]-energy[0])*.5
+val03 = -2*np.imag(val03)/(muC-muL)*(energy[1]-energy[0])*.5
 val1 = -2*np.imag(val1)/(muR-muL)
 val2 = -2*np.imag(val2)/(muR-muL)
 val3 = -2*np.imag(val3)/(muR-muL)
 val4 = -2*np.imag(val4)/(muR-muL)
+
+plt.plot(val01)
+plt.show(block=False)
+
+plt.plot(val02)
+plt.show(block=False)
+
+plt.plot(val03)
+plt.show(block=False)
 
 plt.plot(val1)
 plt.show(block=False)
